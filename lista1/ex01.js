92b9e1f3a218d9ace1da5b0e34f9102d82e34543
Kauan720 <kauan.americo.santos@escola.pr.gov.br>
2026-06-15 20:19:23 +0000

    up

---
+++ lista1/ex01.js
@@ -0,0 +1,7 @@
+// Temperatura em Celsius
+let celsius = 25;
+
+// Conversão para Fahrenheit
+let fahrenheit = celsius * 1.8 + 32;
+// Exibindo com Template String
+console.log(`A temperatura de ${celsius}°C equivale a ${fahrenheit}°F.`);
