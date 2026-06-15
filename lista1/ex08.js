858c7a9bf4bcf043632b5505eb9c015f626f2b96
Kauan720 <kauan.americo.santos@escola.pr.gov.br>
2026-06-15 20:43:20 +0000

    up

---
+++ lista1/ex08.js
@@ -0,0 +1,15 @@
+let tamanho = 8;
+let tabuleiro = "";
+
+for (let linha = 0; linha < tamanho; linha++) {
+  for (let coluna = 0; coluna < tamanho; coluna++) {
+    if ((linha + coluna) % 2 === 0) {
+      tabuleiro += " ";
+    } else {
+      tabuleiro += "#";
+    }
+  }
+  tabuleiro += "\n";
+}
+
+console.log(tabuleiro);
