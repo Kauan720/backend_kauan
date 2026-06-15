f9d707ef16d8c2422bd5289a82a4548539c9c77a
Kauan720 <kauan.americo.santos@escola.pr.gov.br>
2026-06-15 20:27:32 +0000

    up

---
+++ lista1/ex02.js
@@ -0,0 +1,11 @@
+// Declaração das variáveis
+let base = 10;
+let altura = 5;
+
+// Cálculos
+let area = base * altura;
+let perimetro = 2 * (base + altura);
+
+// Saída usando Template Strings
+console.log(`Área do retângulo: ${area}`);
+console.log(`Perímetro do retângulo: ${perimetro}`);
