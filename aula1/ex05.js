d8c5c61702d2ccd0082b8ab5acb4271406f52af4
Kauan720 <kauan.americo.santos@escola.pr.gov.br>
2026-07-06 20:55:32 +0000

    ex05.js

---
+++ aula1/ex05.js
@@ -0,0 +1,17 @@
+const input = require('readline-sync');
+
+let pares = 0;
+let impares = 0;
+
+for (let i = 1; i <= 10; i++) {
+    let numero = Number(input.question(`Digite o ${i}° número: `));
+
+    if (numero % 2 === 0) {
+        pares++;
+    } else {
+        impares++;
+    }
+}
+
+console.log(`Pares: ${pares}`);
+console.log(`Ímpares: ${impares}`);
